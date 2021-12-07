10eb90b78efc60ebf8810ee991b72fb95927f2c4
Sebastian Pech <sebastian.pech@me.com>
2021-12-07 08:41:59 +0100

    Add day 7

---
+++ 7.jl
@@ -0,0 +1,7 @@
+horizontal_positions = parse.(Int,split(read("7.input", String),","))
+horizontal_positions = parse.(Int,split(read("7-ex-a.input", String),","))
+
+#Part 1
+minimum(sum(abs.(horizontal_positions.-pos)) for pos in minimum(horizontal_positions):maximum(horizontal_positions))
+#Part 2
+minimum(mapreduce(n->n*(n+1)÷2, +, abs.(horizontal_positions.-pos)) for pos in minimum(horizontal_positions):maximum(horizontal_positions))
